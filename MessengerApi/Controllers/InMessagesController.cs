75940da6acd1976707d9bfa8f2fcbaf2cfcd0f61
Alexander Grevtsev <alexander88.88@mail.ru>
2020-05-15 11:13:01 +0300

    Добавлен контроллер для обработки сообщений

---
+++ MessengerApi/Controllers/InMessagesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Messenger.BLL.Interfaces;
+using Messenger.Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MessengerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InMessagesController : ControllerBase
+    {
+        private readonly IMessageService _messageService;
+
+        public InMessagesController(IMessageService messageService)
+        {
+            _messageService = messageService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<InMessage>> Get()
+        {
+            return await _messageService.GetMessagesAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<InMessage>> PostMessage(InMessage message)
+        {
+            if (message == null || string.IsNullOrEmpty(message.MessageText))
+                return BadRequest();
+
+            message.RecivedAt = DateTime.Now;
+            message.IpAdress = ControllerContext.HttpContext.Connection.RemoteIpAddress.ToString();            
+            await _messageService.AddMessageAsync(message);
+            return CreatedAtAction("PostMessage", new { id = message.Id }, message);
+        }
+    }
+}
