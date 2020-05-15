4585fa8f44a4289de27ba7eee84225081ea1763d
Alexander Grevtsev <alexander88.88@mail.ru>
2020-05-15 10:12:11 +0300

    Добавлен интерфейс и реализация репозитория

---
+++ Messenger.DAL/Interfaces/IRepository.cs
@@ -0,0 +1,29 @@
+﻿using Messenger.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Messenger.DAL.Interfaces
+{
+    public interface IRepository<TContext, T>
+        where TContext : DbContext
+        where T : EntityBase
+    {
+        IEnumerable<T> GetAll();
+        Task<IEnumerable<T>> GetAllAsync();
+        T Get(int id);
+        Task<T> GetAsync(int id);
+        IEnumerable<T> Find(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = "");
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = "");
+        void Create(T item);
+        void Update(T item);
+        void Delete(int id);
+        void Delete(T entityToDelete);
+
+        void Save();
+        Task SaveAsync();
+    }
+}
---
+++ Messenger.DAL/Repositories/Repository.cs
@@ -0,0 +1,151 @@
+﻿using Messenger.Core.Entities;
+using Messenger.DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Messenger.DAL.Repositories
+{
+    public class Repository<TContext, T> : IRepository<TContext, T>
+        where TContext : DbContext
+        where T : EntityBase
+    {
+        internal TContext context;
+        internal DbSet<T> dbSet;
+
+        public Repository(TContext context)
+        {
+            this.context = context;
+            dbSet = context.Set<T>();
+        }
+
+        public virtual IEnumerable<T> GetAll()
+        {
+            return Find();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await FindAsync();
+        }
+
+        public T Get(int id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public async Task<T> GetAsync(int id)
+        {
+            return await dbSet.FindAsync(id);
+        }
+
+        public IEnumerable<T> Find(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = "")
+        {
+            IQueryable<T> query = dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            foreach (var includeProperty in includeProperties.Split
+                (new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+
+            if (orderBy != null)
+            {
+                return orderBy(query).ToList();
+            }
+            else
+            {
+                return query.ToList();
+            }
+        }
+
+        public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = "")
+        {
+            IQueryable<T> query = dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            foreach (var includeProperty in includeProperties.Split
+                (new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+
+            if (orderBy != null)
+            {
+                return await orderBy(query).ToListAsync();
+            }
+            else
+            {
+                return await query.ToListAsync();
+            }
+        }
+
+        public void Create(T item)
+        {
+            dbSet.Add(item);
+        }
+
+        public void Update(T item)
+        {
+            dbSet.Attach(item);
+            context.Entry(item).State = EntityState.Modified;
+        }
+
+        public void Delete(int id)
+        {
+            var entityToDelete = dbSet.Find(id);
+            Delete(entityToDelete);
+        }
+
+        public virtual void Delete(T item)
+        {
+            if (context.Entry(item).State == EntityState.Detached)
+            {
+                dbSet.Attach(item);
+            }
+            dbSet.Remove(item);
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        public async Task SaveAsync()
+        {
+            await context.SaveChangesAsync();
+        }
+
+        private bool disposed = false;
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+                this.disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
