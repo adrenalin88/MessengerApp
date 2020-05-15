872172b0431ec01a7df5992420f437b11d39eaf9
Alexander Grevtsev <alexander88.88@mail.ru>
2020-05-15 10:21:24 +0300

    Добавлен класс результата отправки сообщения на сервер

---
+++ Messenger.BLL/MessageSendResult.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+
+namespace Messenger.BLL
+{
+    public class MessageSendResult
+    {
+        public HttpStatusCode ResponseCode { get; set; }
+        public Exception Exception { get; set; }
+
+        public bool Sucsess => Exception == null && ResponseCode == HttpStatusCode.Created;
+
+        public static MessageSendResult FromResponseStatusCode(HttpStatusCode responseCode)
+        {
+            return new MessageSendResult() { ResponseCode = responseCode };
+        }
+        public static MessageSendResult FromException(Exception exception)
+        {
+            return new MessageSendResult() { Exception = exception };
+        }
+        public static MessageSendResult BeforeSend()
+        {
+            return new MessageSendResult();
+        }
+    }
+}
