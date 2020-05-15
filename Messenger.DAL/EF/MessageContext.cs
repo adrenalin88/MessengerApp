bbb97787c6c99704a9e2b6a2a1b29b9fdaa74f23
Alexander Grevtsev <alexander88.88@mail.ru>
2020-05-15 09:50:34 +0300

    Добавлен контекст данных

---
+++ Messenger.DAL/EF/MessageContext.cs
@@ -0,0 +1,20 @@
+﻿using Messenger.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Messenger.DAL.EF
+{
+    public class MessageContext<T> : DbContext
+        where T : MessageBase
+    {
+        public DbSet<T> Messages { get; set; }
+        public MessageContext(DbContextOptions<MessageContext<T>> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
