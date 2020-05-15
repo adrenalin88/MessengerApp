784b655549f6cf00701a9c58ba36a72d9235c49d
Alexander Grevtsev <alexander88.88@mail.ru>
2020-05-15 12:49:12 +0300

    Добавлено внедрение зависимостей и подключение к БД

--- MessengerClient/Program.cs
+++ MessengerClient/Program.cs
@@ -1,12 +1,45 @@
-﻿using System;
+﻿using Messenger.BLL.Interfaces;
+using Messenger.BLL.Services;
+using Messenger.Core.Entities;
+using Messenger.DAL.EF;
+using Messenger.DAL.Interfaces;
+using Messenger.DAL.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Configuration;
 
 namespace MessengerClient
 {
     class Program
     {
+        private static IServiceProvider _serviceProvider;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            RegisterServices();
+            DisposeServices();
+        }
+
+        private static void RegisterServices()
+        {
+            var collection = new ServiceCollection();
+            collection.AddDbContext<MessageContext<OutMessage>>(options =>
+                options.UseSqlServer(ConfigurationManager.ConnectionStrings["ClientMessageDB"].ToString()), ServiceLifetime.Transient);
+            collection.AddScoped(typeof(IRepository<,>), typeof(Repository<,>));
+            collection.AddScoped<IClientMessageService, ClientMessageService>();
+            collection.AddScoped<IMessageSender, MessageSender>();
+            _serviceProvider = collection.BuildServiceProvider();
+        }
+        private static void DisposeServices()
+        {
+            if (_serviceProvider == null)
+            {
+                return;
+            }
+            if (_serviceProvider is IDisposable)
+            {
+                ((IDisposable)_serviceProvider).Dispose();
+            }
         }
     }
 }
